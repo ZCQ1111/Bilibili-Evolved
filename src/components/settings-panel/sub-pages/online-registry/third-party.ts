09fe977425a74b0a33a82b420265ed274d813a37
ZCQ1111 <zcqmtq@gmail.com>
2023-03-16 15:20:01 +0800

    改一下更新源和在线仓库分支

--- src/components/settings-panel/sub-pages/online-registry/third-party.ts
+++ src/components/settings-panel/sub-pages/online-registry/third-party.ts
@@ -1,6 +1,6 @@
 import { registerAndGetData } from '@/plugins/data'
 
-const defaultRegistryBranches = ['master', 'preview']
+const defaultRegistryBranches = ['master', 'preview', 'rankinglist-homepage']
 export const [registryBranches] = registerAndGetData('settingsPanel.registryBranches', [
   ...defaultRegistryBranches,
 ])
